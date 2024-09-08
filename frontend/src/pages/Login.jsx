e9e30e1b7d390464f3f134ebe284b1c96b901f0b
HamzaHussain123 <hamza.hussain0504@gmail.com>
2024-09-08 12:28:35 +0500

    login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../hooks/useLogin'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState("")
+
+    const { login, loading } = useLogin()
+
+    const handleSubmit = async (e) => {
+        e.preventDetault()
+
+        await login(email, password)
+    }
+
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-200'>
@@ -9,7 +22,7 @@ const Login = () => {
                     <span className='text-blue-600'> Chat Application</span>
                 </h1>
 
-                <form className='space-y-8 relative z-10'>
+                <form className='space-y-8 relative z-10' onSubmit={handleSubmit}>
 
                     <div className='relative' >
                         <label className='label p-2'>
@@ -18,7 +31,12 @@ const Login = () => {
                             </span>
 
                         </label>
-                        <input type="email" placeholder='Enter your Email' className='w-full input input-bordered h-12 px-4 rounded-lg border-2 border-gray-300 focus:border-blue-500 transition-transform duration-300 ease-out transform hover:scale-105 focus:scale-105 shadow-sm focus:outline-none' />
+                        <input type="email" placeholder='Enter your Email' className='w-full input input-bordered h-12 px-4 rounded-lg border-2 border-gray-300 focus:border-blue-500 transition-transform duration-300 ease-out transform hover:scale-105 focus:scale-105 shadow-sm focus:outline-none'
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}
+                        />
                     </div>
 
                     <div className='relative'>
@@ -28,7 +46,12 @@ const Login = () => {
                             </span>
 
                         </label>
-                        <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-12 px-4 rounded-lg border-2 border-gray-300 focus:border-blue-500 transition-transform duration-300 ease-out transform hover:scale-105 focus:scale-105 shadow-sm focus:outline-none' />
+                        <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-12 px-4 rounded-lg border-2 border-gray-300 focus:border-blue-500 transition-transform duration-300 ease-out transform hover:scale-105 focus:scale-105 shadow-sm focus:outline-none'
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                        />
                     </div>
 
                     <Link to={"/signup"} className='text-sm hover:underline text-blue-600 mt-1 inline-block cursor-pointer'>
@@ -37,8 +60,12 @@ const Login = () => {
                     </Link>
 
                     <div>
-                        <button className='btn btn-block  bg-blue-600 hover:bg-blue-700 text-white font-bold  rounded-lg transition-transform duration-300 ease-out transform hover:scale-105 shadow-lg'>
-                            Login
+                        <button className='btn btn-block  bg-blue-600 hover:bg-blue-700 text-white font-bold  rounded-lg transition-transform duration-300 ease-out transform hover:scale-105 shadow-lg' disabled={loading}>
+                            {loading ? (
+                                <span className='loading loading-spinner'></span>
+                            ) : (
+                                "Login"
+                            )}
                         </button>
                     </div>
 
